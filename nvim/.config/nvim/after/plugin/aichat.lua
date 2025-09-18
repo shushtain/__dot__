2580536306c53327ad53fe225aefaafe534adb85
Artem Shush <shushtain@gmail.com>
2025-09-18 18:58:34 +0300

    refactor(nvim): async aichat calls

--- nvim/.config/nvim/after/plugin/aichat.lua
+++ nvim/.config/nvim/after/plugin/aichat.lua
@@ -1,30 +1,37 @@
 local function ai_expand_line(role)
   local prompt = vim.api.nvim_get_current_line()
-  local result = ""
-
   if role == "%code%" then
     prompt = "[" .. vim.bo.filetype .. "]\n" .. prompt
   end
-
   prompt = vim.fn.shellescape(prompt)
-  result = vim.fn.system("aichat -r " .. role .. " " .. prompt)
 
-  if vim.v.shell_error ~= 0 then
-    vim.notify("aichat returned error code", vim.log.levels.WARN)
-    return 1
-  end
+  vim.system(
+    { "aichat", "-r", role, vim.fn.trim(prompt) },
+    { text = true },
+    function(obj)
+      if obj.code ~= 0 then
+        vim.schedule(function()
+          vim.notify(
+            obj.code .. "/" .. obj.signal .. ": " .. obj.stderr,
+            vim.log.levels.WARN
+          )
+        end)
+        return
+      end
 
-  local rows = vim.split(result, "\n")
-
-  local comment = vim.o.commentstring
-  comment = comment:gsub("%%s", "AI")
-  for i, row in ipairs(rows) do
-    if row:match("```") then
-      rows[i] = comment
+      local result = vim.split(obj.stdout, "\n")
+      vim.schedule(function()
+        local comment = vim.o.commentstring
+        comment = comment:gsub("%%s", "AI")
+        for i, row in ipairs(result) do
+          if row:match("```") then
+            result[i] = comment
+          end
+        end
+        vim.api.nvim_put(result, "l", true, false)
+      end)
     end
-  end
-
-  vim.api.nvim_put(rows, "l", true, false)
+  )
 end
 
 local function ai_improve(retry)
@@ -32,18 +39,15 @@ local function ai_improve(retry)
   if not retry then
     request = vim.fn.input({ prompt = "Request: " })
   end
-
   if not request then
     return nil
   end
 
   vim.g.u_last_prompt = request
-
   if request:gsub("%s", "") == "" then
     request =
       "Rewrite it into a clear, grammatically correct version while preserving the original meaning as closely as possible. Correct any spelling mistakes, punctuation errors, verb tense issues, word choice problems, and other grammatical mistakes. Keep the original paragraph structure, formatting quirks, etc"
   end
-
   local ft = vim.bo.filetype
   request = request .. " [" .. ft .. "]"
 
@@ -52,20 +56,30 @@ local function ai_improve(retry)
   prompt = "::: " .. request .. " :::\n" .. prompt
   prompt = vim.fn.shellescape(prompt)
 
-  local result = vim.fn.system("aichat -r improve " .. prompt)
+  vim.system(
+    { "aichat", "-r", "improve", vim.fn.trim(prompt) },
+    { text = true },
+    function(obj)
+      if obj.code ~= 0 then
+        vim.schedule(function()
+          vim.notify(
+            obj.code .. "/" .. obj.signal .. ": " .. obj.stderr,
+            vim.log.levels.WARN
+          )
+        end)
+        return
+      end
 
-  if vim.v.shell_error ~= 0 then
-    vim.notify("aichat returned error code", vim.log.levels.WARN)
-    return 1
-  end
-
-  local rows = vim.split(result, "\n")
-
-  local buf = vim.api.nvim_create_buf(true, true)
-  vim.bo[buf].filetype = ft
-  vim.api.nvim_buf_set_lines(buf, 0, -1, false, rows)
-  vim.api.nvim_open_win(buf, true, { split = "right" })
-  vim.cmd("windo diffthis")
+      local result = vim.split(obj.stdout, "\n")
+      vim.schedule(function()
+        local buf = vim.api.nvim_create_buf(true, true)
+        vim.bo[buf].filetype = ft
+        vim.api.nvim_buf_set_lines(buf, 0, -1, false, result)
+        vim.api.nvim_open_win(buf, true, { split = "right" })
+        vim.cmd("windo diffthis")
+      end)
+    end
+  )
 end
 
 vim.keymap.set("n", "<Leader>ic", function()
