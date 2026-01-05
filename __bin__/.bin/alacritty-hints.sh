e375d8063cb81c58979bbd23e3365dbc45fefd3b
Artem Shush <shushtain@gmail.com>
2026-01-05 03:40:30 +0200

    fix: xdg-open

--- __bin__/.bin/alacritty-hints.sh
+++ __bin__/.bin/alacritty-hints.sh
@@ -2,18 +2,4 @@
 
 wl-copy "$*"
 dunstify -t 1000 -r 8080 -u low "Opening" "$*"
-
-if [[ "$*" == "http"* ]]; then
-    xdg-open "$*"
-elif [[ "$*" == "file://"* ]]; then
-    path="$(echo "$*" | sed 's/file:\/\///')"
-    type="$(xdg-mime query filetype "$path")"
-
-    if [[ $type == "inode/directory" ]]; then
-        alacritty -e yazi "$path"
-    else
-        alacritty -e xdg-open "$*"
-    fi
-else
-    alacritty -e xdg-open "$*"
-fi
+alacritty -e xdg-open "$*"
--- __bin__/.bin/xdg-handler-file.sh
+++ __bin__/.bin/xdg-handler-file.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 path=${1#file://}
-exec xdg-open "$path"
+exec nvim "$path"
