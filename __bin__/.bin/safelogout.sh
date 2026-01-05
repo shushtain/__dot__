eb935ec1c401f9d63875ee144bdaee4de6eaadba
Artem Shush <shushtain@gmail.com>
2026-01-05 21:08:56 +0200

    feat(hypr)!: no uwsm

--- __bin__/.bin/safelogout.sh
+++ __bin__/.bin/safelogout.sh
@@ -4,7 +4,7 @@ status="$(hyprctl -j clients | jq -c ".[]" | wc -l)"
 busy="$(dropbox-cli status)"
 if [[ $status == 0 ]]; then
     if [[ $busy == "Up to date" ]]; then
-        loginctl terminate-user ""
+        hyprctl dispatch exit
     elif [[ $busy == "Dropbox isn't running!" ]]; then
         dunstify -t 3000 -r 8080 -u low "Starting Dropbox"
         dropbox-cli start
