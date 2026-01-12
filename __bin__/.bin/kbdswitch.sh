a1c058cb62c60e7fbb06c669433dee3143346410
Artem Shush <shushtain@gmail.com>
2026-01-12 15:01:43 +0200

    refactor(hypr): kbdswitch

--- __bin__/.bin/kbdswitch.sh
+++ __bin__/.bin/kbdswitch.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
-hyprctl switchxkblayout at-translated-set-2-keyboard next
-layout="$(hyprctl devices -j | jq -r '.keyboards[].active_keymap' | sort | tail -n1)"
-layout="${layout% (Shush)}"
-dunstify -t 500 -r 8080 -u low "⌨ $layout"
+device="at-translated-set-2-keyboard"
+index="${1:-next}"
+
+hyprctl switchxkblayout $device "$index"
+if [[ $2 != "silent" ]]; then
+    layout=$(hyprctl devices -j | jq -r ".keyboards[] | select(.name == \"$device\") | .active_keymap")
+    layout="${layout% (Shush)}" # (*
+    dunstify -t 500 -r 8080 -u low "⌨ $layout"
+fi
